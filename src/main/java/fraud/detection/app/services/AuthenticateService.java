e100d17864c39c089f05d77a70179aa072cf8a20
Phillip <phillipmugambi68@gmail.com>
2023-03-10 17:06:05 +0300

    update

--- src/main/java/fraud/detection/app/services/AuthenticateService.java
+++ src/main/java/fraud/detection/app/services/AuthenticateService.java
@@ -7,6 +7,7 @@ import fraud.detection.app.repositories.UserRepository;
 import fraud.detection.app.responses.UniversalResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,63 +17,45 @@ import org.springframework.stereotype.Service;
 import java.util.concurrent.ExecutionException;
 
 @Slf4j
-@RequiredArgsConstructor
 @Service
 public class AuthenticateService {
-    private final JwtTokenUtil jwtTokenUtil;
-    private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository;
+    @Autowired
+    private  JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    private  UserRepository userRepository;
+    public UniversalResponse response;
     public UniversalResponse login(AuthDTO request) {
+
 try {
-    Authentication authentication = authenticationManager.authenticate(SecurityContextHolder.getContext().setAuthentication(authentication);
-    String jwt = jwtTokenUtil.createToken(request.getMobileNumber());
-    User user = userRepository.findUserByMobileNumber(request.getMobileNumber());
+    Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(request.getMobileNumber(), request.getPassword()));
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+}catch (Exception ex){
+    UniversalResponse response = UniversalResponse.builder()
+                        .message("Username or Password incorrect")
+                        .build();
+                return response;
+}
     try {
+        User user = userRepository.findUserByMobileNumber(request.getMobileNumber());
         if (user == null) {
             UniversalResponse response = UniversalResponse.builder()
                     .message("User not found, Please Register")
                     .build();
             return response;
         } else {
+            String jwt = jwtTokenUtil.createToken(request.getMobileNumber());
+            response.setMessage(jwt);
             UniversalResponse response = UniversalResponse.builder()
                     .message(jwt)
                     .build();
             return response;
+
         }
     }catch (Exception ex){
         System.out.println(ex);
     }
-}catch (Exception ex){
-    System.out.println(ex);
-
-}
-
-
-
-//        var gentoken = "";
-//        try {
-//            var user = userRepository.findUserByMobileNumber(request.getMobileNumber());
-//                if (user != null) {
-//                    log.info("user found");
-//                       authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-//                               request.getMobileNumber(), request.getPassword()
-//                        ));
-//                        gentoken = jwtTokenUtil.createToken(user);
-//
-////                        System.out.println("UsernamePassword{}"+ex);
-////                    }
-//                }
-//            }catch (Exception ex){
-//                UniversalResponse response = UniversalResponse.builder()
-//                        .message("User not found, Please Register")
-//                        .build();
-//                return response;
-//            }
-//
-//    }
-        UniversalResponse response = UniversalResponse.builder()
-                .message("ytytytyty")
-                .build();
         return response;
 }
 
